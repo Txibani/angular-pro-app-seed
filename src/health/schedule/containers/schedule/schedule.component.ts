bc80deeee9bfccda3fa1d62e90a906e6cd2c0964
Maite Gracia <maitegracia223@gmail.com>
2019-03-30 18:32:48 +0100

    Auth guards to reactive forms

---
+++ src/health/schedule/containers/schedule/schedule.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'schedule',
+    styleUrls: ['schedule.component.scss'],
+    template: `
+        <h1>Schedule</h1>
+    `
+})
+
+export class ScheduleComponent {
+    constructor() {}
+}
---
+++ src/health/workouts/containers/workouts/workouts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'workouts',
+    styleUrls: ['workouts.component.scss'],
+    template: `
+        <h1>WORKOUTS</h1>
+    `
+})
+
+export class WorkoutsComponent {
+    constructor() {}
+}
